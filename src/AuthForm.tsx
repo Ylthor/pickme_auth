98b4e909f90d05858492f3ac4100fbbeb7f9c8be
mashkevich.dima@bk.ru <mashkevich.dima@bk.ru>
2025-12-01 19:20:33 +0500

    test translations

--- src/AuthForm.tsx
+++ src/AuthForm.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import AuthFormInners from "./components/auth/AuthFormInners";
 import "./styles/globals.css";
+import './i18n/config.ts';
 export function AuthForm() {
     return (
         <AuthFormInners/>
